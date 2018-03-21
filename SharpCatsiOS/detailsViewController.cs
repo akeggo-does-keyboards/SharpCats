84d427f7bc20e370042980e4cfe863790297963a
Akeggo <gordonl2@Learn.cssd.ab.ca>
2018-03-21 10:52:07 -0600

    One day..
    I log on and see my githubs want me to delet things so I say yes

---
+++ SharpCatsiOS/detailsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace SharpCatsiOS
+{
+    public partial class detailsViewController : UIViewController
+    {
+        public detailsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
